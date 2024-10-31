6b898912a9754f5b5d618b819de4e683e30985ee
donpoulp <raphael.petrozzi@le-campus-numerique.fr>
2024-10-31 15:15:43 +0100

    Raph - modify user and mise en place eureka serveur et eureka client pour user

--- eureka_server/src/main/java/com/example/eureka_server/DTO/User.java
+++ eureka_server/src/main/java/com/example/eureka_server/DTO/User.java
@@ -1,39 +1,39 @@
-package com.example.eureka_server.DTO;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import java.sql.Date;
-import java.io.Serializable;
-
-@Getter @Setter @NoArgsConstructor
-public class User {
-    private int id;
-
-    @NotBlank
-    private String firstName;
-
-    @NotBlank
-    private String lastName;
-
-    @NotNull
-    private Date birthDate;
-
-    @NotNull
-    private String permitCode;
-
-    @NotNull
-    private Date dateOfObtaining;
-
-
-    public User(int id, String firstName, String lastName, String code, int years, int months, int days) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthDate = new Date(years, months, days);
-        this.permitCode = code;
-    }
-}
+//package com.example.eureka_server.DTO;
+//
+//import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.NotNull;
+//import jakarta.validation.constraints.Size;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import java.sql.Date;
+//import java.io.Serializable;
+//
+//@Getter @Setter @NoArgsConstructor
+//public class User {
+//    private int id;
+//
+//    @NotBlank
+//    private String firstName;
+//
+//    @NotBlank
+//    private String lastName;
+//
+//    @NotNull
+//    private Date birthDate;
+//
+//    @NotNull
+//    private String permitCode;
+//
+//    @NotNull
+//    private Date dateOfObtaining;
+//
+//
+//    public User(int id, String firstName, String lastName, String code, int years, int months, int days) {
+//        this.id = id;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.birthDate = new Date(years, months, days);
+//        this.permitCode = code;
+//    }
+//}
--- eureka_server/src/main/java/com/example/eureka_server/RestTemplateUser.java
+++ eureka_server/src/main/java/com/example/eureka_server/RestTemplateUser.java
@@ -1,39 +1,39 @@
-package com.example.eureka_server;
-
-import com.example.eureka_server.DTO.User;
-import jakarta.ws.rs.HttpMethod;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-
-@Component
-public class RestTemplateUser implements CommandLineRunner {
-
-    @Autowired
-    private RestTemplate restTemplate = restTemplate();
-
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        ResponseEntity<List<User>> reponse = this.restTemplate.exchange(
-                        "http://USER/users",
-                        HttpMethod.GET,
-                        null,
-                        new ParameterizedTypeReference<List<User>>() {
-                        });
-
-        reponse.getBody().forEach(System.out::println);
-    }
-}
+//package com.example.eureka_server;
+//
+//import com.example.eureka_server.DTO.User;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.core.ParameterizedTypeReference;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.client.RestTemplate;
+//
+//import java.util.List;
+//
+//@Component
+//public class RestTemplateUser implements CommandLineRunner {
+//
+//    @Autowired
+//    private RestTemplate restTemplate = restTemplate();
+//
+//    @Bean
+//    @LoadBalanced
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        ResponseEntity<List<User>> reponse = this.restTemplate.exchange(
+//                        "http://USER/users",
+//                        HttpMethod.GET,
+//                        null,
+//                        new ParameterizedTypeReference<List<User>>() {
+//                        });
+//
+//        reponse.getBody().forEach(System.out::println);
+//    }
+//}
