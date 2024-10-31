5f143e514c4d8addc1aa8481c72883cdda089ce0
Jocelyn Grelier <grelierjocelyn@gmail.com>
2024-10-31 15:09:54 +0100

    vehicle controller model CRUD API jo

---
+++ vehicle/src/main/java/com/example/vehicle/controller/VehicleController.java
@@ -0,0 +1,95 @@
+package com.example.vehicle.controller;
+
+import com.example.vehicle.model.Vehicle;
+import com.example.vehicle.repository.VehicleRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@Validated
+@RequestMapping(path = "/api") // route /api par default
+public class VehicleController {
+
+    private final VehicleRepository vehicleRepository;
+
+
+    public VehicleController(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+    /**
+     * Get all vehicle
+     *
+     * @return
+     */
+    @GetMapping(value = "/vehicle")
+    public Iterable<Vehicle> findAll() {
+        return vehicleRepository.findAll();
+    }
+
+    /**
+     * Get one Vehicle by vehicle_registration
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/vehicle/{id}")
+    public Optional<Vehicle> getVehicleById(@PathVariable int id) {
+        return vehicleRepository.findById(id);
+    }
+
+    /**
+     * Create on Vehicle
+     *
+     * @param vehicle
+     * @return
+     */
+    @PostMapping(value = "/vehicle")
+    public ResponseEntity<Vehicle> addVehicle(@RequestBody Vehicle vehicle) {
+        return ResponseEntity.ok(vehicleRepository.save(vehicle));
+    }
+
+    /**
+     * Update Vehicle by Id
+     *
+     * @param id
+     * @param vehicle
+     * @return
+     */
+    @PutMapping(value = "/vehicle/{id}")
+    public ResponseEntity<String> updateVehicle(@PathVariable int id, @RequestBody Vehicle vehicle) {
+        Optional<Vehicle> vehicleOptional = vehicleRepository.findById(id);
+        if (vehicleOptional.isPresent()) {
+            vehicle.setId(id);
+            vehicle.setVehicle_registration(vehicle.getVehicle_registration());
+            vehicle.setVehicleType(vehicle.getVehicleType());
+            vehicle.setBrand(vehicle.getBrand());
+            vehicle.setModel(vehicle.getModel());
+            vehicle.setColor(vehicle.getColor());
+            vehicle.setReservationPrice(vehicle.getReservationPrice());
+            vehicle.setKmPrice(vehicle.getKmPrice());
+            vehicle.setTaxHorse(vehicle.getTaxHorse());
+            vehicle.setMaintenanceStartDate(vehicle.getMaintenanceStartDate());
+            vehicle.setMaintenanceEndDate(vehicle.getMaintenanceEndDate());
+            vehicle.setTraveledKm(vehicle.getTraveledKm());
+            vehicle.setCylinder(vehicle.getCylinder());
+            vehicle.setVolume(vehicle.getVolume());
+            vehicleRepository.save(vehicle);
+            return new ResponseEntity<String>("Vehicle updated", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Vehicle not found", HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @DeleteMapping(value = ("/vehicle/{id}"))
+    public ResponseEntity<String> deleteVehicle(@PathVariable int id) {
+        vehicleRepository.deleteById(id);
+        return new ResponseEntity<>("Vehicle deleted", HttpStatus.OK);
+    }
+
+}
---
+++ vehicle/src/main/java/com/example/vehicle/model/Vehicle.java
@@ -0,0 +1,62 @@
+package com.example.vehicle.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "vehicle")
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+    @Column(name="vehicle_registration", nullable = false, unique = true)
+    private String vehicle_registration;
+
+    @Column(name="vehicle_type", nullable = false)
+    private String vehicleType;
+
+    @Column(name="brand", nullable = false)
+    private String brand;
+
+    @Column(name="model", nullable = false)
+    private String model;
+
+    @Column(name="color", nullable = false)
+    private String color;
+
+    @Column(name="reservation_price", nullable = false)
+    private Float reservationPrice;
+
+    @Column(name="km_price", nullable = false)
+    private Float kmPrice;
+
+    @Column(name="tax_horse", nullable = false)
+    private Float taxHorse;
+
+    @Column(name="maintenance_start_date", nullable = false)
+    private Date maintenanceStartDate;
+
+    @Column(name="maintenance_end_date", nullable = false)
+    private Date maintenanceEndDate;
+
+    @Column(name="traveled_km", nullable = false)
+    private int traveledKm;
+
+    @Column(name="cylinder", nullable = true)
+    private int cylinder;
+
+    @Column(name="volume", nullable = true)
+    private int volume;
+
+}
